980eeefa08edd111d937db0abe0915b1c38af32a
Ryo Shibayama <ryo.shibayama@gree.co.jp>
2015-06-27 23:24:13 +0900

    refactoring

--- batch.php
+++ batch.php
@@ -1,30 +1,71 @@
 <?php
 date_default_timezone_set('Asia/Tokyo');
-$countries = array('jp', 'us', 'cn', 'kr');
-$source_filename_prefix = array('topfree', 'toppaid', 'topgrossing');
-$source_dir = "./src";
-$output_dir = "./data";
+
+class AppleRankingXmlDownloader
+{
+    const SRC_DIR = "./src";
+    const OUTPUT_DIR = "./data";
+
+    private $countries = [
+        'jp',
+        'us',
+        'cn',
+        'kr',
+    ];
+
+    private $srcPrefix = [
+        'topfree',
+        'toppaid',
+        'topgrossing',
+    ];
 
 // データ格納用ディレクトリが存在しなければ作る
-if (!is_dir($output_dir)) mkdir($output_dir, 0777);
-foreach ($countries as $country) {
-	$chk_dir = $output_dir . '/' . $country;
-	if (!is_dir($chk_dir)) {
-		mkdir($chk_dir, 0777);
-	}
+    public function makeOutputDir()
+    {
+        if (!is_dir(self::OUTPUT_DIR)) {
+            mkdir(self::OUTPUT_DIR, 0777);
+        }
+
+        foreach ($this->countries as $country) {
+            $chkDir = self::OUTPUT_DIR . '/' . $country;
+            if (!is_dir($chkDir)) {
+                mkdir($chkDir, 0777);
+            }
+        }
+    }
+
+    private function getInputFilename($country, $srcPrefix)
+    {
+        return sprintf("%s/%s_%s.list", self::SRC_DIR, $country, $srcPrefix);
+    }
+
+    private function getWgetCommand($xmlUrl, $outputFilename)
+    {
+        return sprintf("wget %s -O %s", $xmlUrl, $outputFilename);
+    }
+
+    private function getOutputFilename($country, $srcPrefix, $category)
+    {
+        return sprintf("%s/%s/%s_%s_%s.xml", self::OUTPUT_DIR, $country, $srcPrefix, $category, date('Ymd'));
+    }
+
+    public function downloadXml()
+    {
+        foreach ($this->countries as $country) {
+            foreach ($this->srcPrefix as $srcPrefix) {
+                $contents = @file($this->getInputFilename($country, $srcPrefix));
+                foreach ($contents as $line) {
+                    list($category, $xmlUrl) = explode("," ,$line);
+                    $category = trim($category);
+                    $xmlUrl = trim($xmlUrl);
+                    $outputFilename = $this->getOutputFilename($country, $srcPrefix, $category);
+                    system($this->getWgetCommand($xmlUrl, $outputFilename));
+                }
+            }
+        }
+    }
 }
 
-foreach ($countries as $country) {
-	foreach ($source_filename_prefix as $filename_prefix) {
-		$filename = $source_dir . '/' . $country . '_' . $filename_prefix . '.list';
-		$contents = @file($filename);
-		foreach ($contents as $line) {
-			$rss_data = explode("," ,$line);
-			$category = trim($rss_data[0]);
-			$xml_url = trim($rss_data[1]);
-			$output_filename = $output_dir . '/' . $country . '/' . $filename_prefix . '_' . $category . '_' . date('Ymd') . '.xml';
-			$cmd = "wget " . $xml_url . " -O " . $output_filename;
-			system($cmd);
-		}
-	}
-}
+$downloader = new AppleRankingXmlDownloader();
+$downloader->makeOutputDir();
+$downloader->downloadXml();
